e384a9e34a47400d40ca9e265a40b1a95c01a9de
vindictive-s <se7bastianb.bosoi@gmail.com>
2023-02-16 16:53:30 +0000

    dashboard app geo

--- server/index.js
+++ server/index.js
@@ -12,7 +12,15 @@ import salesRoutes from "./routes/sales.js";
 
 // data imports
 import User from "./models/User.js";
-import { dataUser } from "./data/index.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+} from "./data/index.js";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -42,6 +50,9 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /* ONLY ADD DATA ONE TIME */
+    //Product.insertMany(dataProduct);
+    //ProductStat.insertMany(dataProductStat);
+    //Transaction.insertMany(dataTransaction);
     // User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
